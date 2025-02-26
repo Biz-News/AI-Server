aecc30d6b69b25ebd00399d00ef5551113591346
이상원 <simon.lee@isang-won-ui-MacBookPro.local>
2025-02-27 07:32:54 +0900

    feat/DB class

---
+++ app/DB.py
@@ -0,0 +1,49 @@
+import os
+import mysql.connector
+from mysql.connector import errorcode
+from dotenv import load_dotenv
+from news import News
+
+# 어떤 query가 필요하나
+# 1. keywords 전체
+# 2. 뉴스 id 여러개로 한번에
+
+
+class DB:
+    def __init__(self):
+        load_dotenv()
+        user = os.getenv("DB_USER")
+        password = os.getenv("DB_PASSWORD")
+        name = os.getenv("DB_NAME")
+        host = os.getenv("DB_HOST")
+        db_config = {
+            "host": host,
+            "user": user,
+            "password": password,
+            "database": name,
+        }
+        self.cnx = mysql.connector.connect(**db_config)
+        self.cursor = self.cnx.cursor()
+        
+    def get_every_keyword(self):
+        sql = "SELECT * FROM keyword" # TODO keyword 테이블 이름 바뀌면 수정
+        self.cursor.execute(sql)
+        result = self.cursor.fetchall()
+        keywords = [keyword_tuple[0] for keyword_tuple in result]
+        return keywords
+    
+    def get_news_by_ids(self, news_ids: list):
+        sql = f"SELECT * FROM news WHERE news_id IN ({', '.join(map(str, news_ids))})" # TODO news 테이블 이름 바뀌면 수정
+        self.cursor.execute(sql)
+        result = self.cursor.fetchall()
+        news = [News(*news_tuple) for news_tuple in result] # TODO news가 어떻게 생겼냐에 따라 수정
+        return news
+
+    def close(self):
+        self.cursor.close()
+        self.cnx.close()
+        
+if __name__ == '__main__':
+    db = DB()
+    db.cursor.execute('SELECT * FROM news')
+    print(db.cursor.fetchall())
--- app/news.py
+++ app/news.py
@@ -1,20 +1,30 @@
 from dataclasses import dataclass
 from datetime import datetime
 
+
 @dataclass
 class News:
-  news_id: int
-  # url: str
-  title: str
-  sub_title: str
-  content: str
-  # date: datetime
+    news_id: int
+    url: str
+    title: str
+    sub_title: str
+    article_text: str
+    date: datetime
 
-  def __str__(self):
-    return f"""
+    def to_json(self):
+        return {
+            "news_id": self.news_id,
+            "url": self.url,
+            "title": self.title,
+            "sub_title": self.sub_title,
+            "article_text": self.article_text,
+            "date": self.date
+        }
+    
+    def __str__(self):
+        return f"""
   ## 뉴스 ID : {self.news_id}
   제목 : {self.title}
   소제목 : {self.sub_title}
-  내용 : {self.content}
+  내용 : {self.article_text}
   """
-  
